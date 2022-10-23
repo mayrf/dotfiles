ad91ec976e65a39a54849bee87d66a4e5315c1c3
Fritz Mayr <f.mayr@hotmail.com>
2022-10-23 17:44:38 +0200

    Add Lazygit toggle chortcut

--- nvim/.config/nvim/lua/user/keymaps.lua
+++ nvim/.config/nvim/lua/user/keymaps.lua
@@ -66,6 +66,9 @@ keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
 keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
 
 
+keymap("n", "<leader>lg", "<cmd>lua _LAZYGIT_TOGGLE()<cr>", opts)
+
+
 -- Telescope
 -- keymap("n", "<leader>f", "<cmd>Telescope find_files<cr>", opts)
 keymap("n", "<leader>f", "<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({ previewer = false }))<cr>", opts)
