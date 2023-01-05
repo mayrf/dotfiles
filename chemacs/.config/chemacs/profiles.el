b8b50bf25b05e77372f95a8ca725d0c01c5782f7
Fritz Mayr <f.mayr@d3eif.com>
2023-01-05 23:12:14 +0100

    Add chemacs submodule

---
+++ chemacs/.config/chemacs/profiles.el
@@ -0,0 +1,4 @@
+(("default" . ((user-emacs-directory . "~/dotfiles/emacs/.config/emacs-configs/efs")))
+ ("spacemacs" . ((user-emacs-directory . "~/dotfiles/emacs/.config/emacs-configs/spacemacs")
+                 (env . (("SPACEMACSDIR" . "~/dotfiles/emacs/.config/emacs-configs/.spacemacs.env")))))
+ )
