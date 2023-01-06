91f8cdc090caa0fac1f5de136130f011490e6d93
Fritz Mayr <f.mayr@d3eif.com>
2023-01-06 20:58:02 +0100

    Add doom emacs

--- chemacs/.config/chemacs/profiles.el
+++ chemacs/.config/chemacs/profiles.el
@@ -1,4 +1,7 @@
-(("default" . ((user-emacs-directory . "~/.config/emacs-configs/efs")))
- ("spacemacs" . ((user-emacs-directory . "~/.config/emacs-configs/spacemacs")
+(("default"   . ((user-emacs-directory   . "~/.config/emacs-configs/efs")))
+ ("spacemacs" . ((user-emacs-directory   . "~/.config/emacs-configs/spacemacs")
                  (env . (("SPACEMACSDIR" . "~/.config/emacs-configs/spacemacs.d")))))
+ ("doom"      . ((user-emacs-directory   . "~/.config/emacs-configs/doom-emacs")
+                 (env . (("DOOMDIR"      . "~/.config/doom")))))
+
  )
