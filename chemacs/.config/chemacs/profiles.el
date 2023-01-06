fc3f9818dc3a985ff7273e5a8b1a523bd2b04aeb
Fritz Mayr <f.mayr@d3eif.com>
2023-01-06 14:40:55 +0100

    Add spacemacs

--- chemacs/.config/chemacs/profiles.el
+++ chemacs/.config/chemacs/profiles.el
@@ -1,4 +1,4 @@
 (("default" . ((user-emacs-directory . "~/.config/emacs-configs/efs")))
  ("spacemacs" . ((user-emacs-directory . "~/.config/emacs-configs/spacemacs")
-                 (env . (("SPACEMACSDIR" . "~/.config/emacs-configs/.spacemacs.env")))))
+                 (env . (("SPACEMACSDIR" . "~/.config/emacs-configs/spacemacs.d")))))
  )
